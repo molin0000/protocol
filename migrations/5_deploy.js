654fce3c2a0065a53611e5591ba7044beea1a840
Mo Lin <molin@wanchain.org>
2019-09-06 13:52:10 +0800

    update

--- migrations/5_deploy.js
+++ migrations/5_deploy.js
@@ -1,10 +1,9 @@
 const HybridExchange = artifacts.require('HybridExchange');
-const ProxySol = artifacts.require('Proxy');
+const Proxy = artifacts.require('Proxy');
 const TestToken = artifacts.require('TestToken');
 const WethToken = artifacts.require('WethToken');
 
 
 module.exports = async (deployer, network) => {
-
-    await deployer.deploy(ProxySol);
+    await deployer.deploy(WethToken);
 };
